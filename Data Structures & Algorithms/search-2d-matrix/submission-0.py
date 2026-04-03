b09368bf525a465846c525b2a3f64af3bfd13895
abrehman02 <115941928+abrehman02@users.noreply.github.com>
2026-04-03 07:46:56 +0530

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.py
@@ -0,0 +1,43 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # writing a lower bound (index for ele <= target )
+
+        def lowerBound(nums):
+            low = 0
+            high = len(nums) - 1
+
+            def check(mid):
+                if nums[mid] >= target :
+                    return 1
+                else:
+                    return 0
+
+            ans = -1
+            
+            while low <= high :
+                mid = low + (high - low) // 2
+                if check(mid):
+                    ans = mid 
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            
+            return ans
+
+        last_array = []
+
+        for i in range(len(matrix)):
+            last_array.append(matrix[i][-1])
+        
+        lb = lowerBound(last_array)
+
+        if matrix[lb][-1] == target :
+            return True
+        
+        second_array = matrix[lb]
+
+        ans = lowerBound(second_array)
+
+        if matrix[lb][ans] == target :
+            return True
+        return False
