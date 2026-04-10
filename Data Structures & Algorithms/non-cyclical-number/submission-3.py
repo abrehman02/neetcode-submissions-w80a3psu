c03b6386b93cdddfa0a6f5821f0295fbc2b4bda0
abrehman02 <115941928+abrehman02@users.noreply.github.com>
2026-04-11 00:42:52 +0530

    Add: non-cyclical-number - submission-3

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        hashset = set()
+        num = 0
+        while n:
+            r = n % 10
+            num += r*r
+            n //= 10
+            if n == 0 :
+                if num == 1 :
+                    return True
+                else:
+                    if num in hashset :
+                        return False
+                    else:
+                        hashset.add(num)
+                        n = num
+                        num = 0
+            print(hashset)
+        # return True
+                
+        
+    
+
+
