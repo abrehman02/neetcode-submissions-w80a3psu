46ce05a6c7e8ef5761c97086655fb0b8090e77b3
abrehman02 <115941928+abrehman02@users.noreply.github.com>
2026-04-11 11:29:13 +0530

    Update: jump-game - submission-11

--- Data Structures & Algorithms/jump-game/submission-11.py
+++ Data Structures & Algorithms/jump-game/submission-11.py
@@ -1,32 +1,33 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # dp = [-1]*len(nums)
-        # def rec(i):
-        #     if i >= len(nums) - 1 :
-        #         return True
-        #     if nums[i] == 0 :
-        #         return False
-
-        #     if dp[i] != -1 :
-        #         return dp[i]
-
-        #     for t in range(nums[i],0,-1):
-        #         if i+t >= len(nums) - 1 :
-        #             return True
-        #         ans = rec(i+t)
-        #         if ans == True  :
-        #             dp[i] = ans
-        #             return True
-        #     dp[i] = False
-        #     return False
         
-        # return rec(0)
-        goal = len(nums) - 1
-
-        for i in range(len(nums)-2,-1,-1):
-            if i + nums[i] >= goal :
-                goal = i
+        dp = [-1]*len(nums)
         
-        if goal <= 0 :
-            return True
-        return False
+        def rec(i):
+            if i >= len(nums) - 1 :
+                return True
+            if nums[i] == 0 :
+                return False
+
+            if dp[i] != -1 :
+                return dp[i]
+
+            for t in range(nums[i],0,-1):
+                if i+t >= len(nums) - 1 :
+                    return True
+                if rec(i+t):
+                    dp[i] = True
+                    return True
+            dp[i] = False
+            return False
+        
+        return rec(0)
+        # goal = len(nums) - 1
+
+        # for i in range(len(nums)-2,-1,-1):
+        #     if i + nums[i] >= goal :
+        #         goal = i
+        
+        # if goal <= 0 :
+        #     return True
+        # return False
