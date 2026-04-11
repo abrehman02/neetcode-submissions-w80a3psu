3f83453aefea9778c740bdb50c3dc54607c8a102
abrehman02 <115941928+abrehman02@users.noreply.github.com>
2026-04-11 11:07:39 +0530

    Add: jump-game - submission-7

---
+++ Data Structures & Algorithms/jump-game/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        def rec(i):
+            if i >= len(nums) - 1 :
+                return True
+            
+            if nums[i] == 0 :
+                return False
+
+            for t in range(nums[i],0,-1):
+                if i+t >= len(nums) - 1 :
+                    return True
+                else:
+                    ans = rec(i+t)
+                    if ans == True:
+                        return ans
+            
+            return False
+        
+        return rec(0)
